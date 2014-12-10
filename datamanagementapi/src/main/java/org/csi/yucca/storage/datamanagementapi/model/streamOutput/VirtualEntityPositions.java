f4c8c5d55db9041d521cf8afec31b57daf462d66
afranceschetti <alessandro.franceschetti@csi.it>
2014-12-10 14:54:36 +0100

    CSV download stream fields

---
+++ datamanagementapi/src/main/java/org/csi/yucca/storage/datamanagementapi/model/streamOutput/VirtualEntityPositions.java
@@ -0,0 +1,18 @@
+package org.csi.yucca.storage.datamanagementapi.model.streamOutput;
+
+public class VirtualEntityPositions {
+	private Position position;
+
+	public VirtualEntityPositions() {
+		super();
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+}
