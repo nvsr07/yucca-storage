e5d8c76250caa7369706341af7027d7d01d70471
Eric Locci <eric.locci@consulenti.csi.it>
2017-06-05 17:27:10 +0200

    Script Mongo to CSV

--- mongo-hadoop-alignment/mongo-download/allinea_preprod.sh
+++ mongo-hadoop-alignment/mongo-download/allinea_preprod.sh
@@ -1,7 +1,37 @@
-cd /home/oozie/scripts
-./scarico_tenant.sh preprod
+#!/bin/bash
+
+eexit() {
+    local error_str="$@"
+    echo $error_str
+    exit 1
+}
+
+cd /home/sdp/allinea_yucca
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   Start procedure allinea_preprod "
+echo ""
+echo "Verifica istanza unica"
+exec 200>/home/sdp/.allinea_preprod.lock
+flock -n 200 || eexit "Procedura allinea_preprod gia in esecuzione"
+#
+echo "acquisisce keytab"
+kinit -kt /etc/security/keytabs/sdp.service.keytab -p sdp/sdnet-master3.sdp.csi.it@SDP.CSI.IT
+#
+data_corr=`date +%Y%m%d_%H%M`
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   lancio ./scarico_tenant_convergenza.sh preprod "
+./scarico_tenant_convergenza.sh preprod >>/home/sdp/allinea_yucca/log/allinea_preprod_${data_corr}.log 2>>/home/sdp/allinea_yucca/log/allinea_preprod_${data_corr}.log
 if [ $? -eq 0 ]
 then
-./upload.sh preprod
+  echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   lancio ./upload_convergenza.sh preprod "
+  ./upload_convergenza.sh preprod >>/home/sdp/allinea_yucca/log/allinea_preprod_${data_corr}.log 2>>/home/sdp/allinea_yucca/log/allinea_preprod_${data_corr}.log
+  if [ $? -ne 0 ]
+  then
+    echo " !!!!!    ERRORE    !!!!!  "
+  fi
+else
+  echo " !!!!!    ERRORE    !!!!!  "
 fi
-
+echo ""
+echo "Elimina file di lock"
+rm /home/sdp/.allinea_preprod.lock
+#
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   End procedure allinea_preprod "
--- mongo-hadoop-alignment/mongo-download/allinea_prod.sh
+++ mongo-hadoop-alignment/mongo-download/allinea_prod.sh
@@ -1,7 +1,38 @@
-cd /home/oozie/scripts
-./scarico_tenant.sh prod
+#!/bin/bash
+
+eexit() {
+    local error_str="$@"
+    echo $error_str
+    exit 1
+}
+
+
+cd /home/sdp/allinea_yucca
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   Start procedure allinea_prod "
+echo ""
+echo "Verifica istanza unica"
+exec 200>/home/sdp/.allinea_prod.lock
+flock -n 200 || eexit "Procedura allinea_prod gia in esecuzione"
+#
+echo "acquisisce keytab"
+kinit -kt /etc/security/keytabs/sdp.service.keytab -p sdp/sdnet-master3.sdp.csi.it@SDP.CSI.IT
+#
+data_corr=`date +%Y%m%d_%H%M`
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   lancio ./scarico_tenant_convergenza.sh prod "
+./scarico_tenant_convergenza.sh prod >>/home/sdp/allinea_yucca/log/allinea_prod_${data_corr}.log 2>>/home/sdp/allinea_yucca/log/allinea_prod_${data_corr}.log
 if [ $? -eq 0 ]
 then
-./upload.sh prod
+  echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   lancio ./upload_convergenza.sh prod "
+  ./upload_convergenza.sh prod >>/home/sdp/allinea_yucca/log/allinea_prod_${data_corr}.log 2>>/home/sdp/allinea_yucca/log/allinea_prod_${data_corr}.log
+  if [ $? -ne 0 ]
+  then
+    echo " !!!!!    ERRORE    !!!!!  "
+  fi
+else
+  echo " !!!!!    ERRORE    !!!!!  "
 fi
-
+echo ""
+echo "Elimina file di lock"
+rm /home/sdp/.allinea_prod.lock
+#
+echo "`date +%H.%M.%S` ---- `date +'%a, %d %h %Y'` ----   End procedure allinea_prod "
